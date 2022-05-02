147b624435e032cc5098cb88b88253994e05e354
LaV0n <102952888+LaV0n@users.noreply.github.com>
2022-05-02 13:29:05 +0200

    1

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-const url = 'https://api.unsplash.com/search/photos?query=spring&per_page=30&orientation=landscape&client_id=SouHY7Uul-OxoMl3LL3c0NkxUtjIrKwf3tsGk1JaiVo';
+const url = 'https://api.unsplash.com/search/photos?query=spring&per_page=1&orientation=landscape&client_id=SouHY7Uul-OxoMl3LL3c0NkxUtjIrKwf3tsGk1JaiVo';
 async function  getData() {
 const res = await fetch(url);
 const data = await res.json();
-console.log(data.urls.regular);
+console.log( data.urls.regular);
 }
-//getData();
-const img = document.createElement('img');
+getData();
+/*const img = document.createElement('img');
 img.classList.add('gallery-img')
 img.src = `getData()`;
 img.alt = `image`;
-galleryContainer.append(img);
+galleryContainer.append(img);*/
